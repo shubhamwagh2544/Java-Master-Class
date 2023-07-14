9252b4ec1ba0bfe86b3422e89c02a5b1ad715744
Shubham Wagh <shubhamwagh2544@gmail.com>
2023-07-14 23:45:45 +0530

    Serialization and Deserialization

---
+++ src/main/java/com/javamasterclass/serialization/ExternalizableImplementation.java
@@ -0,0 +1,66 @@
+package com.javamasterclass.serialization;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.io.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Getter
+@ToString
+class Teacher implements Externalizable {
+    private String name;
+    private Integer age;
+    private transient LocalDateTime dateOfBirth;
+
+    @Override
+    public void writeExternal(ObjectOutput out) throws IOException {
+        out.writeObject(name);
+        out.writeInt(age);
+    }
+
+    @Override
+    public void readExternal(ObjectInput in) throws IOException, ClassNotFoundException {
+        String name = (String) in.readObject();
+        Integer age = in.readInt();
+    }
+}
+
+/*
+        Externalization :
+        Serialization but in more controlled/customizable way
+
+        Externalizable interface
+        - writeExternal() => for serializing
+        - readExternal() => for deserializing
+
+        transient => hides/excludes the attribute from serialization process
+        dateOfBirth will be null in deserialization state => since it was not passed for serialization
+ */
+
+public class ExternalizableImplementation {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        File file = new File("src/main/java/com/javamasterclass/serialization/externalizable_file.txt");
+
+        //serialization
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+        //object serialized into byte stream and saved to file.txt
+        Teacher teacher = new Teacher("Shubham Wagh", 25, LocalDateTime.now());
+        System.out.println(teacher);                            //before serializing object
+        objectOutputStream.writeObject(teacher);
+
+        //deserialization
+        FileInputStream fileInputStream = new FileInputStream(file);
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+        //byte stream deserialized into object
+        Teacher teacher1 =(Teacher) objectInputStream.readObject();
+        System.out.println(teacher1);                           //after serializing object
+    }
+}
---
+++ src/main/java/com/javamasterclass/serialization/SerializationImplementation.java
@@ -0,0 +1,58 @@
+package com.javamasterclass.serialization;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.ToString;
+
+import java.io.*;
+import java.time.LocalDateTime;
+
+@AllArgsConstructor
+@Getter
+@ToString
+class Student implements Serializable {
+    private String name;
+    private Integer age;
+    private transient LocalDateTime dateOfBirth;
+}
+
+/*
+        Serialization : [Persistence of object state]
+        - way to convert object into byte stream, to store in db/disc, transmit over network, save/persist to file.
+
+        Deserialization :
+        - way to convert byte stream into object [reconstruct object from its serialized form]
+        
+        How :
+        implementing => Serializable(marker interface) OR Externalizable(which extends Serializable)
+
+        Serializable : default serialization logic/process
+        Externalizable : custom serialization logic (more control over serialization process)
+
+        transient => hides/excludes the attribute from serialization process
+        dateOfBirth will be null in deserialization state => since it was not passed for serialization
+*/
+
+public class SerializationImplementation {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
+        File file = new File("src/main/java/com/javamasterclass/serialization/serializable_file.txt");
+
+        //serialization
+        FileOutputStream fileOutputStream = new FileOutputStream(file);
+        ObjectOutputStream objectOutputStream = new ObjectOutputStream(fileOutputStream);
+
+        //object serialized into byte stream and saved to file.txt
+        Student student = new Student("Shubham Wagh", 25, LocalDateTime.now());
+        System.out.println(student);                            //before serializing object
+        objectOutputStream.writeObject(student);
+
+        //deserialization
+        FileInputStream fileInputStream = new FileInputStream(file);
+        ObjectInputStream objectInputStream = new ObjectInputStream(fileInputStream);
+
+        //byte stream deserialized into object
+        Student student1 =(Student) objectInputStream.readObject();
+        System.out.println(student1);                           //after serializing object
+
+    }
+}
