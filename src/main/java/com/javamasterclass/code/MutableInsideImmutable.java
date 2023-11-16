c3dfb775fd868653bdfe4daea1aac219cf6cd2f5
shubham.wagh <shubhamwagh2544@gmail.com>
2023-11-16 12:40:11 +0530

    Mutable Object inside Immutable Object

---
+++ src/main/java/com/javamasterclass/code/MutableInsideImmutable.java
@@ -0,0 +1,35 @@
+package com.javamasterclass.code;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
+public final class MutableInsideImmutable {     // final to prevent behavior change
+    private List<String> list;                  //mutable object field
+
+    public MutableInsideImmutable(List<String> list) {
+        this.list = new ArrayList<>(list);
+    }
+
+    //no setters
+
+    public List<String> getList() {
+        return Collections.unmodifiableList(list);
+    }
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();      // list is mutable
+        list.add("Shubham");
+        list.add("Ramesh");
+        list.add("Wagh");
+
+        MutableInsideImmutable immutable = new MutableInsideImmutable(list);    //object is immutable
+
+        //mutable
+        list.add("Testing Mutability");
+
+        //immutable : UnSupportedOperationException
+        immutable.getList().add("Testing Immutability");        //UnSupportedOperation Exception
+    }
+}
